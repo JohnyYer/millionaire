9f58444d55929753d99f1b958b4fcd49daf80d67
Ivan Yeromenko <i.yeromenko@sloboda-studio.com>
2023-12-15 15:56:18 +0200

    finish page

--- src/app/finish/page.tsx
+++ src/app/finish/page.tsx
@@ -1,11 +1,27 @@
-import Link from "next/link";
+import Image from "next/image";
+import ButtonLink from "@/components/ButtonLink";
+import "./page.scss";
 
 function Finish() {
   return (
-    <div>
-      <p>Total score:</p>
-      <h1>$8,000 earned</h1>
-      <Link href="/">Try again</Link>
+    <div className="finish-page">
+      <div className="finish-page__image">
+        <Image
+          src="/thumbsUp.svg"
+          alt="Thumbs Up"
+          fill
+          // sizes="(max-width: 768px) 300px"
+        />
+      </div>
+
+      <div className="finish-page__right">
+        <div className="finish-page__right-score">
+          <p>Total score:</p>
+          <h1>$8,000 earned</h1>
+        </div>
+
+        <ButtonLink href="/" text="Try again" />
+      </div>
     </div>
   );
 }
