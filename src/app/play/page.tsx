85b52b54291c0de88dfae0d826d69d738f27bede
Ivan Yeromenko <i.yeromenko@sloboda-studio.com>
2023-12-15 14:48:44 +0200

    Answer block component

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,5 +1,45 @@
+import AnswerBlock from "@/components/AnswerBlock";
+import "./page.scss";
+
 function Play() {
-  return <div>Play</div>;
+  const answers = [
+    {
+      text: "10 years",
+      isCorrect: true,
+    },
+    {
+      text: "10 years",
+      isCorrect: true,
+    },
+    {
+      text: "10 years",
+      isCorrect: true,
+    },
+    {
+      text: "10 years",
+      isCorrect: true,
+    },
+  ];
+
+  return (
+    <div className="play-area">
+      <div className="play-area__board">
+        <h2 className="play-area__board-question">
+          How old your elder brother was 10 years before you was born, mate?
+        </h2>
+
+        <div className="play-area__board-answers">
+          {answers.map((answer) => (
+            <div key={answer.text} className="play-area__board-answers-block">
+              <AnswerBlock type="correct" />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="play-area__score-sidebar">score</div>
+    </div>
+  );
 }
 
 export default Play;
